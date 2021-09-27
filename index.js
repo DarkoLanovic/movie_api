ef59872fa6c56f47ee31e41ace8e5479e45a3b7d
Darko Lanovic <lanovic.darko@gmail.com>
2021-09-27 13:56:42 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 const express = reqiire('express');
 const app = express;
+
+let top10Movies = [
+    {
+        title: 'Amadeus'
+    },
+    {
+        title: 'Citizen Kane'
+    },
+    {
+        title: 'Raging Bull'
+    },
+    {
+        title: 'Mean Streets'
+    },
+    {
+        title: 'Once Upon a Time In America'
+    },
+    {
+        title: 'Scent of a Woman'
+    },
+    {
+        title: 'Reservoir Dogs'
+    },
+    {
+        title: 'Casablanca'
+    },
+    {
+        title: 'Casino'
+    },
+    {
+        title: 'Taxi Driver'
+    }
+];
+
+app.get('/movies', (req, res) => {
+    res.jsnon(top10Movies);
+});
