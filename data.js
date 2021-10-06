091b2697444830ccb65e65d14b55adbe49ccb81a
Darko Lanovic <lanovic.darko@gmail.com>
2021-10-06 09:37:19 -0400

    initial commit

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
-let users = [
+const { application } = require("express");
+
+const users = [
     {
         id:1,
         fullname: "U",
@@ -44,7 +46,7 @@ let users = [
 ];
 
 
-let movies = [
+const movies = [
     {
         title: "Amadeus",
         actors: "...",
@@ -107,10 +109,48 @@ let movies = [
         director: "..."
     }
 ];
+
+const genre = [
+    {
+        name: "action",
+        description: "Movie that typically include violence, extended fighting, physical feats, rescues and frantic chases"
+    },
+    {
+        name: "triller",
+        description: "Thriller is a genre of fiction, having numerous, often overlapping subgenres. Thrillers are characterized and defined by the moods they elicit, giving viewers heightened feelings of suspense, excitement, surprise, anticipation and anxiety."
+    },
+    {
+        name: "drama",
+        description: "Drama is a category of narrative fiction intended to be more serious than humorous in tone."
+    },
+    {
+        name: "comedy",
+        description: "Movie in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect."
+    },
+    {
+        name: "romance",
+        description: "Romance movies are romantic love stories recorded in visual media for broadcast in theaters and on TV that focus on passion, emotion, and the affectionate romantic involvement of the main characters and the journey that their love takes them through dating, courtship or marriage."
+    },
+    {
+        name: "horror",
+        description: "A horror film is one that seeks to elicit fear or disgust in its audience for entertainment purposes."
+    },
+    {
+        name: "animated",
+        description: "Movie consisting of a sequence of drawings, each slightly different so that when filmed and run through a projector the figures seem to move."
+    }
+];
+
+const directors = [
+    {
+
+    }
+]
       
 module.exports = {
     users,
-    movies
+    movies,
+    genre,
 }
 
 
