6b46e0e6741a9217ad9bf5ab509cc7bd75453919
Darko Lanovic <lanovic.darko@gmail.com>
2021-10-15 10:21:18 -0400

    initial commit

--- models.js
+++ models.js
@@ -1,1 +1,37 @@
 const mongoose = require('mongoose');
+
+// Creating the models(This will create collections called “db.movies” and “db.users” within the MongoDB)
+let Movie = mongoose.model('Movie', movieShema);
+let User = mongoose.model('User', userShema);
+
+
+let movieShema = mongoose.Schema({
+    Title: {type: String, require: true},
+    Descrption: {type: String, require:true},
+    Genre: {
+        Name: String,
+        Descrption: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Reatured: Boolean
+
+});
+
+let userShema = mongoose.Schema({
+    Usrname: {type: String, require: true},
+    Password: {type: String, require:true},
+    Email: {type: String, require: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieShema);
+let User = mongoose.model('User', userShema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
