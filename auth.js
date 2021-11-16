4d2f082bb0ab799916400c0970173a171668d712
Darko Lanovic <lanovic.darko@gmail.com>
2021-11-16 13:20:27 -0500

    initial commit

--- auth.js
+++ auth.js
@@ -15,7 +15,7 @@ let generateJWTToken = (user) => {
 }
 
 
-/* POST login. */
+// POST login
 module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
