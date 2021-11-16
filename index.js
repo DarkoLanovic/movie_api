9413cac3ec2615d927f9507bc8cf84d3671ae3d6
Darko Lanovic <lanovic.darko@gmail.com>
2021-11-16 13:41:48 -0500

    initial commit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 
 
 // GET ALL JASON MOVIES DATA
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
